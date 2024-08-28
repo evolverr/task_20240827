f798dfbc06936fb6afc7a51ae4d400cac0a96ecd
김중용(Payment) <joongyong.kim@myrealtrip.com>
2024-08-28 16:38:16 +0900

    first commit

---
+++ src/main/java/com/clovirtualfashion/homework/CloApplication.java
@@ -0,0 +1,13 @@
+package com.clovirtualfashion.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CloApplication.class, args);
+  }
+
+}
